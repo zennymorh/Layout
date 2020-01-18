64d10267fc1b8994105c4bb2ce2f06178e326c93
Emmanuel Kehinde <emmakoko1996@gmail.com>
2020-01-18 23:26:21 +0100

    I'm done!!!

---
+++ app/src/main/java/com/zennymorh/layout/BusinessActivity.kt
@@ -0,0 +1,12 @@
+package com.zennymorh.layout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BusinessActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_business)
+    }
+}
