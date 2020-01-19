15885265429f2c54a91906eea6cc56d09fb9edda
Emmanuel Kehinde <emmakoko1996@gmail.com>
2020-01-19 18:32:29 +0100

    okay, now i'm done. I finished the three layouts, used data binding to bind the views i needed. made some final touchups, manually refactored my code.

--- app/src/main/java/com/zennymorh/layout/MainActivity.kt
+++ app/src/main/java/com/zennymorh/layout/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.zennymorh.layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.zennymorh.layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.loginButton.setOnClickListener {
+            login(it)
+        }
+
     }
+
+    private fun login(view: View){
+        val intent = Intent(this, WeatherActivity::class.java)
+        startActivity(intent)
+    }
+
+
 }
--- app/src/main/java/com/zennymorh/layout/WeatherActivity.kt
+++ app/src/main/java/com/zennymorh/layout/WeatherActivity.kt
@@ -1,12 +1,30 @@
 package com.zennymorh.layout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.zennymorh.layout.databinding.ActivityMainBinding
+import com.zennymorh.layout.databinding.ActivityWeatherBinding
 
 class WeatherActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWeatherBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_weather)
+        binding.next.setOnClickListener{
+            move(it)
+        }
+
+    }
+
+    private fun move(view: View){
+        val intent = Intent(this, BusinessActivity::class.java)
+        startActivity(intent)
     }
 }
